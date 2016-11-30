d8b88157524f35750ca6e016ee2641fc36fd9bc5
qbit27 <quoc.bao.cf@gmail.com>
2016-11-30 20:42:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+  if (req.query['hub.verify_token'] === 'Quoc_Bao_yeu_Bao_Quyen_vo_doi') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
